8807d0adb803a9442c903f860726c5f9038d4989
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-19 19:24:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
+    # @number = params[:number].to_i
+    # @phrase = params[:phrase]
     erb :say
   end
 
