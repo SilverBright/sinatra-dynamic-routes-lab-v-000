c8da7b5a692b32104a84b2ffbd8becb74e660862
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-19 19:09:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ class App < Sinatra::Base
   # Write your code here!
 
   get '/reversename/:name' do
-    @reversename = params[:name].reverse
-    "#{name}"
+    @name = params[:name].reverse
+    "#{@name}"
   end
 
 end
