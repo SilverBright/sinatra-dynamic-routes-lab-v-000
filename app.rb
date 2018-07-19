d5005985572aee5c2c5ee1c81ff55e713d18e790
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-19 19:37:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
    end
 
    get '/:operation/:number1/:number2' do
-     "#{params[:number1]} * #{params[:number2]}"
+     "#{params[:number1]} + #{params[:number2]}"
    end
 
 
